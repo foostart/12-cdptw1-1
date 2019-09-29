0049724094fd811c4d47f9e3317f06f200d52369
nguyenhoangminhhieu <mhieubt6@gmail.com>
2019-09-29 17:55:37 +0700

    connit

--- 1159/1159.php
+++ 1159/1159.php
@@ -1,16 +1,16 @@
 
 <?php
-    $url_host = 'http://' . $_SERVER['HTTP_HOST'];
-    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
-    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
-    preg_match_all($pattern_uri, __DIR__, $matches);
-    $url_path = $url_host . $matches[1][0];
-    $url_path = str_replace('\\', '/', $url_path);
-    if (!class_exists('lessc')) {
-        include ('./libs/lessc.inc.php');
-    }
-    $less = new lessc;
-    $less->compileFile('less/1159.less', 'css/1159.css');
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+if (!class_exists('lessc')) {
+    include ('./libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/1159.less', 'css/1159.css');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,16 +20,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>1159</title>
         <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo $url_path?>/css/bootstrap.min.css" rel="stylesheet" style="text-css"/>
-        <link rel="stylesheet" href="<?php echo $url_path?>/css/bootstrap-theme/3.3.7.min.css"/>
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="<?php echo $url_path ?>/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
         <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
-        <script src=" <?php echo $url_path?> bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="<?php echo $url_path ?> /fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <script src=" <?php echo $url_path ?>/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="<?php echo $url_path ?>/css/1159.css">
         <script src="<?php echo $url_path ?>/js/owl.carousel.min.js"></script>
         <script src="<?php echo $url_path ?>/js/1159.js"></script>
     </head>
     <body>
-        <?php include './1159-content.php';?>
+        <?php include './1159-content.php'; ?>
     </body>
 </html>
